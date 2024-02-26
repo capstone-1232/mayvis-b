48f31dcc1fac9b0edced1ccdfe2166438cb72a5c
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-25 21:41:46 -0700

    Removed useless built-in columns

--- database/migrations/2024_02_18_051213_create_proposals_table.php
+++ database/migrations/2024_02_18_051213_create_proposals_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('proposals', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
         });
     }
 
--- database/migrations/2024_02_18_051222_create_products_table.php
+++ database/migrations/2024_02_18_051222_create_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
         });
     }
 
