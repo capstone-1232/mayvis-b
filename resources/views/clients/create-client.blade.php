6cf059f8aee8399fe9a23a4de38499c89cc842b3
icacristobal <icacristobal@gmail.com>
2024-04-10 14:06:19 -0600

    removed pagination after searching/filtering

--- resources/views/clients/create-client.blade.php
+++ resources/views/clients/create-client.blade.php
@@ -1,49 +1,49 @@
 <x-layout>
     <div class="content">
         <div class="container my-3">
-            <div class="row justify-content-center">
+            <div class="row justify-content-center my-4">
                 <div class="col-lg-8">
                     <h2 class="display-6 py-2 fw-bold">
-                        <i class="bi bi-person-fill me-3"></i>Add a Client
+                        <i class="bi bi-person-plus me-3"></i>Add a Client
                     </h2>
-                    <div class="bg-white p-4 rounded-4 mt-2">
+                    <div class="bg-white p-4 rounded-5 shadow mt-4">
                         <form action="{{ route('clients.storeClient') }}" method="POST">
                             @csrf
                             <!-- Form fields for Client Creation -->
-                            <div class="form-group mb-3">
-                                <label for="company_name">Company Name</label>
+                            <div class="form-group mb-3 mt-2">
+                                <x-input-label for="company_name" :value="__('Company Name')" />
                                 <x-text-input type="text" name="company_name" field="company_name" placeholder="Company Name" class="form-control" autocomplete="off" :value="old('company_name')"></x-text-input>
-                                <x-input-error class="mt-2 productserr" :messages="$errors->get('company_name')" />
+                                
                             </div>
     
                             <div class="form-group mb-3">
-                                <label for="first_name">First Name</label>
+                                <x-input-label for="first_name" :value="__('First Name')" />
                                 <x-text-input type="text" name="first_name" placeholder="John" class="form-control" :value="old('first_name')"></x-text-input>
                                 <x-input-error class="mt-2 productserr" :messages="$errors->get('first_name')" />
                             </div>
             
                             <div class="form-group mb-3">
-                                <label for="last_name">Last Name</label>
+                                <x-input-label for="last_name" :value="__('Last Name')" />
                                 <x-text-input type="text" name="last_name" placeholder="Doe" class="form-control" :value="old('last_name')"></x-text-input>
                                 <x-input-error class="mt-2 productserr" :messages="$errors->get('last_name')" />
                             </div>
             
                             <div class="form-group mb-3">
-                                <label for="email">Email</label>
+                                <x-input-label for="email" :value="__('Email')" />
                                 <x-text-input type="email" name="email" placeholder="johndoe@email.com" field="email" class="form-control" autocomplete="off" :value="old('email')"></x-text-input>
-                                <x-input-error class="mt-2 productserr" :messages="$errors->get('email')" />
+                                
                             </div>
             
                             <div class="form-group mb-3">
-                                <label for="phone_number">Phone Number</label>
+                                <x-input-label for="phone_number" :value="__('Phone Number')" />
                                 <x-text-input type="tel" name="phone_number" placeholder="123-456-7891" field="phone_number" class="form-control" autocomplete="off" :value="old('phone_number')"></x-text-input>
-                                <x-input-error class="mt-2 productserr" :messages="$errors->get('phone_number')" />
+                                
                             </div>
             
                             <!-- Add other necessary form fields -->
-                            <div class="d-flex justify-content-end align-items-center mt-3">
+                            <div class="d-flex justify-content-end align-items-center mt-4">
                                 <a href="{{ route('index-client') }}" class="fs-7 fw-bold me-2">Cancel</a>
-                                <x-primary-button type="submit" class="btn primary-btn text-white rounded-pill">Save Client</x-primary-button>
+                                <x-primary-button type="submit" class="btn primary-btn rounded-pill">Save Client</x-primary-button>
                             </div>
                             
                         </form>
--- resources/views/services/servicesIndex.blade.php
+++ resources/views/services/servicesIndex.blade.php
@@ -3,12 +3,23 @@
     <script>
         document.addEventListener('DOMContentLoaded', function () {
 
-            // Category Filter Dropdown
+            const paginationContainer = document.getElementById('pagination-container');
             const categorySelect = document.getElementById('category');
+            const searchForm = document.getElementById('searchForm');
+
+            function togglePaginationVisibility() {
+                if (categorySelect.value || searchForm.search_term.value) {
+                    paginationContainer.style.display = 'none';
+                } else {
+                    paginationContainer.style.display = 'block';
+                }
+            }
+
             categorySelect.addEventListener('change', function() {
-                
+                togglePaginationVisibility();
+
                 const categoryId = this.value;
-                const filterProductsUrl = "{{ route('services.filterProducts') }}"; // Make sure this route is defined in your web.php
+                const filterProductsUrl = "{{ route('services.filterProducts') }}"; 
 
                 fetch(`${filterProductsUrl}?category_id=${categoryId}`, {
                     headers: {
@@ -28,12 +39,10 @@
                 });
             });
 
-
-   
-
-            // Search Function
-            document.getElementById('searchForm').addEventListener('submit', function(e) {
+            searchForm.addEventListener('submit', function(e) {
                 e.preventDefault();
+                togglePaginationVisibility();
+
                 let searchTerm = document.getElementById('search_term').value;
                 fetch("{{ route('services.searchProducts') }}?search_term=" + searchTerm)
                     .then(response => response.json())
@@ -46,66 +55,75 @@
             });
 
             function renderProducts(data) {
-            const productsContainer = document.getElementById('products-container'); // Ensure this is the ID of your container
-            productsContainer.innerHTML = ''; // Clear existing content
+            const productsContainer = document.getElementById('products-container');
+            productsContainer.innerHTML = ''; 
+
+            function formatDate(dateString) {
+                const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                const date = new Date(dateString);
+                return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+            }
 
             let productsHtml = '';
-data.forEach(product => {
-    const createdAtFormatted = product.created_at; // Adjust this to format date as needed
-    productsHtml += `
-        <div class="mb-3">
-            <div class="card h-100 bg-white border-0 rounded-4 shadow-sm">
-                <div class="card-body d-flex flex-column">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <h5 class="card-title display-8 fw-bold mb-0">
-                                <a href="/services/${product.id}/edit" class="stretched-link text-decoration-none text-dark">
-                                    ${product.product_name}
-                                </a>
-                            </h5>
-                        </div>
-                        <div class="text-end text-secondary fst-italic">
-                            <p class="mb-1">Date Created: ${createdAtFormatted}</p>
-                            <p>Created by: ${product.created_by}</p>
+            data.forEach(product => {
+                const createdAtFormatted = formatDate(product.created_at);
+                const updatedAtFormatted = formatDate(product.updated_at);
+                productsHtml += `
+                    <div class="mb-3">
+                        <div class="card h-100 border rounded-4 shadow-sm custom-hover">
+                            <div class="card-body d-flex flex-column">
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        <h5 class="card-title display-8 fw-bold mb-0">
+                                            <a href="/services/${product.id}/edit" class="stretched-link text-decoration-none text-dark">
+                                                ${product.product_name}
+                                            </a>
+                                        </h5>
+                                    </div>
+                                    <div class="text-end text-secondary fst-italic">
+                                        <p class="mb-1">Date Created: ${createdAtFormatted}</p>
+                                        <p class="mb-1">Updated At: ${updatedAtFormatted}</p>
+                                        <p>Created by: ${product.created_by}</p>
+                                    </div>
+                                </div>
+                                <p class="card-text mt-2">${product.description || product.product_description}</p>
+                                <div class="mt-auto text-end fw-bold">
+                                    $ ${product.price}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <p class="card-text mt-2">${product.description || product.product_description}</p>
-                    <div class="mt-auto text-end fw-bold">
-                        $ ${product.price}
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
-});
-productsContainer.innerHTML = productsHtml;
+                `;
+            });
+            productsContainer.innerHTML = productsHtml;
 
         }
+        togglePaginationVisibility();
     });
     </script>
 
 <div class="content">
-    <div class="container my-3">
+    <div class="container mb-3 mt-2">
         <div class="row justify-content-center">
             <div class="col-lg-10">
         <div class="my-4">
             <div class="d-flex justify-content-between align-items-center">
                 <h2 class="display-6 py-2 fw-bold">
-                    <i class="bi bi-box-seam-fill me-3"></i>Services
+                    <i class="bi bi-boxes me-3"></i>Services
                 </h2>
             </div>
         </div>
 
-        <div class="p-4 rounded-4 bg-blue">
+        <div class="p-4 rounded-5 shadow bg-head">
             <div class="d-flex justify-content-between align-items-center">
-                <h3 class="fs-4 py-2 fw-bold text-white">Categories</h3>
+                <h3 class="fs-4 py-2 fw-semibold text-white">Categories</h3>
                 <div>
                     <a href="{{ route('index-category') }}" class="btn secondary-btn rounded-pill fw-bold px-5">Edit Category List</a>
-                    <a href="{{ route('categories.createCategory') }}" class="btn secondary-btn rounded-pill text-uppercase fw-bold px-5 ms-2">Create New</a>
+                    <a href="{{ route('categories.createCategory') }}" class="btn secondary-btn rounded-pill fw-bold px-5 ms-2">Create New</a>
                 </div>
             </div>
             <form id="filterForm" action="{{ route('services.filterProducts') }}" class="mt-3" method="GET">
-                <select name="category_id" id="category" class="form-select" required>
+                <select name="category_id" id="category" class="form-select rounded-pill" required>
                     <option value="">Select a category</option>
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}" {{ (request()->category_id == $category->id) ? 'selected' : '' }}>
@@ -122,17 +140,17 @@ productsContainer.innerHTML = productsHtml;
             </div>
         @endif
           
-        <div class="bg-gray p-4 rounded-4 mt-3">
+        <div class="bg-white p-4 rounded-5 shadow mt-3">
             <div class="d-flex justify-content-between align-items-center">
-                <h3 class="fs-4 py-2 fw-bold text-dark ">Services</h3>
+                <h3 class="fs-4 py-2 fw-semibold text-dark ">Scope of Work</h3>
                 <div>
-                    <a href="{{ route('services.createProduct') }}" class="btn primary-btn text-white rounded-pill text-uppercase fw-bold px-5 ms-2">Create New</a>
+                    <a href="{{ route('services.createProduct') }}" class="btn primary-btn text-white rounded-pill fw-bold px-5 ms-2">Create New</a>
                 </div>
             </div>
             <div class="container mt-3">
                 <form id="searchForm" action="{{ route('services.searchProducts') }}" method="GET">
                     <div class="input-group mb-4 border-2 rounded-pill bg-white">
-                        <input type="text" id="search_term" name="search_term" class="form-control border-0 rounded-start-pill " placeholder="Search">
+                        <input type="text" id="search_term" name="search_term" class="form-control border-0 rounded-start-pill " placeholder="Search scope of work">
                         <button id="search_button" class="btn text-white primary-btn fw-bold px-5 rounded-pill" type="submit">Search</button>
                     </div>
                 </form>
@@ -140,7 +158,7 @@ productsContainer.innerHTML = productsHtml;
             <div id="products-container" class="container mt-3">
                 @foreach ($products as $product)
                 <div class="mb-3">
-                    <div class="card h-100 bg-white border-0 rounded-4 shadow-sm">
+                    <div class="card h-100 border rounded-4 shadow-sm custom-hover">
                         <div class="card-body d-flex flex-column">
                             <!-- Top row for metadata -->
                             <div class="d-flex justify-content-between">
@@ -152,13 +170,18 @@ productsContainer.innerHTML = productsHtml;
                                     </h5>
                                 </div>
                                 <div class="text-end text-secondary fst-italic">
-                                    <p class="mb-1">Date Created: {{ $product->created_at}}</p> <!-- Adjust date format as needed -->
+                                    <p class="mb-1">Date Created: {{ \Carbon\Carbon::parse($product->created_at)->format('F j, Y') }}</p>
+                                    <p class="mb-1">Updated At: {{ \Carbon\Carbon::parse($product->updated_at)->format('F j, Y') }}</p>
+
+
+
                                     <p>Created by: {{ $product->created_by }}</p>
                                 </div>
                             </div>
                 
                             <!-- Main content -->
-                            <p class="card-text mt-2">{{ $product->product_description }}</p>
+                            <p class="card-text mt-2">{!! $product->product_description !!}
+                            </p>
                 
                             <!-- Bottom row for price, aligns to the bottom right -->
                             <div class="mt-auto text-end fw-bold">
@@ -170,9 +193,11 @@ productsContainer.innerHTML = productsHtml;
                 
                 @endforeach
             </div>
-            <div id="pagination-container">
-                {{ $products->appends(['category_id' => request()->category_id])->links() }}
-            </div>
+                @if(!request()->has('category_id') && !request()->has('search_term'))
+                    <div id="pagination-container">
+                        {{ $products->appends(['category_id' => request()->category_id])->links() }}
+                    </div>
+                @endif
         </div>
         
             </div>
